dc4a04eac00d45d88e952072fe03011fff4f880b
Mario Christopher <marioac.dev@gmail.com>
2024-08-01 11:14:25 -0600

    Adding Topic data via JSON file

--- api/index.tsx
+++ api/index.tsx
@@ -1,47 +1,61 @@
-import { Button, FrameIntent, Frog, TextInput } from 'frog'
-import { devtools } from 'frog/dev'
-import { serveStatic } from 'frog/serve-static'
-import { neynar } from 'frog/hubs'
-import { neynar as neynarMW } from 'frog/middlewares'
-import { handle } from 'frog/vercel'
-import { ButtonName, TState, Topic, TopicState } from './types.js'
-import { topics } from './topic-data.js'
-import { saveTopiCast } from './db.js'
+import { Button, FrameIntent, Frog, TextInput } from "frog";
+import { devtools } from "frog/dev";
+import { serveStatic } from "frog/serve-static";
+import { neynar } from "frog/hubs";
+import { neynar as neynarMW } from "frog/middlewares";
+import { handle } from "frog/vercel";
+import { ButtonName, AppState, Topic, TopicState, Topics } from "./types.js";
+import { saveTopiCast } from "./db.js";
+import topics from "./topic-data.json" with { type: "json" };;
 
 // Uncomment to use Edge Runtime.
 // export const config = {
 //   runtime: 'edge',
 // }
 
-export const app = new Frog<{ State: TState }>({
-  assetsPath: '/',
-  basePath: '/api',
+export const app = new Frog<{ State: AppState }>({
+  assetsPath: "/",
+  basePath: "/api",
   initialState: {
-    frame: 1
+    frame: 1,
   },
-  hub: neynar({ apiKey: 'NEYNAR_FROG_FM' }),
-  verify: 'silent'
+  hub: neynar({ apiKey: "NEYNAR_FROG_FM" }),
+  verify: "silent",
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
-})
+});
 
 app.use(
   neynarMW({
-    apiKey: 'NEYNAR_FROG_FM',
-    features: ['interactor', 'cast'],
-  }));
+    apiKey: "NEYNAR_FROG_FM",
+    features: ["interactor", "cast"],
+  })
+);
+
+app.frame("", async (c) => {
+  return c.res({
+    image: (
+      <div style={{ color: "orange", fontSize: 60, margin: "auto" }}>
+        Invalid TopiCast url. Missing Topic name !
+      </div>
+    )
+  });
+});
 
 app.frame("/:topicName", async (c) => {
   const topicName = c.req.param("topicName");
-  const topic = topics[topicName];
-  const buttonValue = c.buttonValue as ButtonName;
+  const topic = (topics as any as Topics)[topicName];
+  const buttonClicked = c.buttonValue as ButtonName;
   let ansFromPrevFrame = ((c.inputText as string) || "").trim();
-  ansFromPrevFrame = ansFromPrevFrame.split(",").map(s => s.trim()).join(",");  //  Clears spaces
+  ansFromPrevFrame = ansFromPrevFrame
+    .split(",")
+    .map((s) => s.trim())
+    .join(","); //  Clears spaces
 
   let previousFrame = 0;
-  const state = c.deriveState((previousState: TState) => {
+  const appState = c.deriveState((previousState: AppState) => {
     previousFrame = previousState.frame;
-    switch (buttonValue) {
+    switch (buttonClicked) {
       case ButtonName.Next:
       case ButtonName.Resume:
       case ButtonName.Finish:
@@ -54,7 +68,6 @@ app.frame("/:topicName", async (c) => {
     }
   });
 
-  console.log("C", c.var);
   const updatedState: TopicState = {
     fid: c.frameData?.fid as number,
     castId: c.frameData?.castId.fid as number,
@@ -63,79 +76,112 @@ app.frame("/:topicName", async (c) => {
     userName: (c.var as any)?.interactor?.username,
     displayName: (c.var as any)?.interactor?.displayName,
     startDateTime: new Date(),
-    status: 'Started',
-  }
+    status: "Started",
+  };
 
-  if (ansFromPrevFrame && ansFromPrevFrame.length > 0 &&
-    buttonValue == ButtonName.Next ||
-    buttonValue == ButtonName.Check ||
-    buttonValue == ButtonName.Finish) {
+  if (
+    (ansFromPrevFrame &&
+      ansFromPrevFrame.length > 0 &&
+      buttonClicked == ButtonName.Next) ||
+    buttonClicked == ButtonName.Check ||
+    buttonClicked == ButtonName.Finish
+  ) {
     updatedState.itemResponse = {
-      [previousFrame]: ansFromPrevFrame
-    }
+      [previousFrame]: ansFromPrevFrame,
+    };
   }
 
-  if (buttonValue == ButtonName.Finish) {
+  if (buttonClicked == ButtonName.Finish) {
     updatedState.status = "Completed";
     updatedState.endDateTime = new Date();
   }
 
-  if (buttonValue != ButtonName.Resume) {
+  if (buttonClicked != ButtonName.Resume && buttonClicked != ButtonName.Link) {
     await saveTopiCast(topic, updatedState);
   }
 
   return c.res({
-    image: buildImage(topic, state, buttonValue, ansFromPrevFrame),
+    image: buildImage(topic, appState, buttonClicked, ansFromPrevFrame),
     intents: [
-      ...buildPrompt(topic, state, buttonValue),
-      ...buildButtons(topic, state, buttonValue),
+      ...buildPrompt(topic, appState, buttonClicked),
+      ...buildButtons(topic, appState, buttonClicked),
     ],
-  })
-})
+  });
+});
 
-function buildImage(topic: Topic, state: TState, buttonValue: ButtonName, ansFromPrevFrame: string) {
+function buildImage(
+  topic: Topic,
+  state: AppState,
+  buttonClicked: ButtonName,
+  ansFromPrevFrame: string
+) {
   if (!topic) {
-    return <div style={{ color: 'orange', fontSize: 60, margin: 'auto' }}>
-      Invalid Topic !
-    </div>;
+    return (
+      <div style={{ color: "orange", fontSize: 60, margin: "auto" }}>
+        Invalid Topic name !
+      </div>
+    );
   }
 
-  if (buttonValue == ButtonName.Check) {
-    const correctAns = topic.items[state.frame - 1].prompt?.correctAnswers?.join(",");
+  if (buttonClicked == ButtonName.Check) {
+    const correctAns =
+      topic.frames[state.frame - 1].prompt?.correctAnswers?.join(",");
     if (correctAns == ansFromPrevFrame) {
-      return <div style={{ display: 'flex', color: 'green', fontSize: 60, margin: 'auto' }}>
-        Correct Answer - {correctAns}
-      </div>;
+      return (
+        <div
+          style={{
+            display: "flex",
+            color: "green",
+            fontSize: 60,
+            margin: "auto",
+          }}
+        >
+          Correct Answer - {correctAns}
+        </div>
+      );
     } else {
-      return <div style={{ display: 'flex', color: 'orange', fontSize: 60, margin: 'auto' }}>
-        Incorrect. The answer is - {correctAns}
-      </div>;
+      return (
+        <div
+          style={{
+            display: "flex",
+            color: "orange",
+            fontSize: 60,
+            margin: "auto",
+          }}
+        >
+          Incorrect. The answer is - {correctAns}
+        </div>
+      );
     }
   } else {
-    return `/${topic.name}/${state.frame}.png?${Date.now()}`;
+    // return `/${topic.name}/${state.frame}.png?${Date.now()}`;
+    return `/${topic.name}/${state.frame}.png`;
   }
 }
 
-function buildButtons(topic: Topic, state: TState, buttonValue: ButtonName) {
+function buildButtons(topic: Topic, state: AppState, buttonClicked: ButtonName) {
   if (!topic) {
     return [];
   }
 
   let buttons: FrameIntent[] = [];
-  const linkUrl = topic.items[state.frame - 1].moreInfo;
-  const prevButton = <Button value={`${ButtonName.Prev}`}>Prev</Button>
-  const nextButton = <Button value={`${ButtonName.Next}`}>Next</Button>
-  const linkButton = <Button.Link href={`${linkUrl}`}>More Info</Button.Link>
-  const checkAnswerButton = <Button value={`${ButtonName.Check}`}>Check Answer</Button>
-  const resumeButton = <Button value={`${ButtonName.Resume}`}>Resume</Button>
-  const finishButton = <Button value={`${ButtonName.Finish}`}>Finish</Button>
+  const linkUrl = topic.frames[state.frame - 1].externalUrl;
+  const prevButton = <Button value={`${ButtonName.Prev}`}>Prev</Button>;
+  const nextButton = <Button value={`${ButtonName.Next}`}>Next</Button>;
+  const linkButton = <Button.Link href={`${linkUrl}`}>More Info</Button.Link>;
+  const checkAnswerButton = (
+    <Button value={`${ButtonName.Check}`}>Check Answer</Button>
+  );
+  const resumeButton = <Button value={`${ButtonName.Resume}`}>Resume</Button>;
+  const finishButton = <Button value={`${ButtonName.Finish}`}>Finish</Button>;
 
   const isFirstFrame = state.frame == 1;
-  const isFramesInBetween = state.frame > 1 && state.frame < topic.items.length - 1
-  const isSecondToLastFrame = state.frame == topic.items.length - 1;
-  const isLastFrame = state.frame == topic.items.length;
+  const isFramesInBetween =
+    state.frame > 1 && state.frame < topic.frames.length - 1;
+  const isSecondToLastFrame = state.frame == topic.frames.length - 1;
+  const isLastFrame = state.frame == topic.frames.length;
 
-  if (buttonValue == ButtonName.Check) {
+  if (buttonClicked == ButtonName.Check) {
     buttons = [resumeButton];
   } else if (isFirstFrame) {
     buttons = [nextButton];
@@ -147,23 +193,30 @@ function buildButtons(topic: Topic, state: TState, buttonValue: ButtonName) {
     //  No buttons for Last Frame
   }
 
-  if (buttonValue != ButtonName.Check && topic.items[state.frame - 1].prompt && topic.items[state.frame - 1].prompt?.allowCheckAnswer) {
+  if (
+    buttonClicked != ButtonName.Check &&
+    topic.frames[state.frame - 1].prompt &&
+    topic.frames[state.frame - 1].prompt?.allowCheckAnswer
+  ) {
     buttons.push(checkAnswerButton);
   }
 
-  if (topic.items[state.frame - 1].moreInfo) {
+  if (topic.frames[state.frame - 1].externalUrl) {
     buttons.push(linkButton);
   }
   return buttons;
 }
 
-function buildPrompt(topic: Topic, state: TState, buttonValue: ButtonName) {
+function buildPrompt(topic: Topic, state: AppState, buttonClicked: ButtonName) {
+  if (!topic) {
+    return [];
+  }
 
   let textInput: FrameIntent[] = [];
   let placeHolder = "";
 
-  const prompt = topic.items[state.frame - 1].prompt;
-  if (topic && buttonValue != ButtonName.Check && prompt) {
+  const prompt = topic.frames[state.frame - 1].prompt;
+  if (topic && buttonClicked != ButtonName.Check && prompt) {
     if (prompt.prompt) {
       if (prompt.correctAnswers && prompt.correctAnswers.length == 1) {
         placeHolder = `Select one: ${prompt.prompt}`;
@@ -175,14 +228,14 @@ function buildPrompt(topic: Topic, state: TState, buttonValue: ButtonName) {
     } else {
       placeHolder = "Questions or feedback ?";
     }
-    textInput.push(<TextInput placeholder={`${placeHolder}`} />)
+    textInput.push(<TextInput placeholder={`${placeHolder}`} />);
   }
   return textInput;
 }
 // @ts-ignore
-const isEdgeFunction = typeof EdgeFunction !== 'undefined'
-const isProduction = isEdgeFunction || import.meta.env?.MODE !== 'development'
-devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic })
+const isEdgeFunction = typeof EdgeFunction !== "undefined";
+const isProduction = isEdgeFunction || import.meta.env?.MODE !== "development";
+devtools(app, isProduction ? { assetsPath: "/.frog" } : { serveStatic });
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
